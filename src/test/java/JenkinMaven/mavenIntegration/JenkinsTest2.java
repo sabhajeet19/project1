9ed343b9009e6b52b0396712a408be7b8355d850
sabhajeet <vermasabhajeet@gmail.com>
2022-01-19 23:53:08 +0530

    pom.xml file updated

---
+++ src/test/java/JenkinMaven/mavenIntegration/JenkinsTest2.java
@@ -0,0 +1,11 @@
+package JenkinMaven.mavenIntegration;
+
+import org.testng.annotations.Test;
+
+public class JenkinsTest2 {
+	@Test
+	public void testCase() {
+		System.out.println("This is second class");
+	}
+
+}
