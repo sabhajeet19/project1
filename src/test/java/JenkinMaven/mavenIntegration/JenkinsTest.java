58c4fab97aefa305f2a9e439a10fc7973e53cce8
LENOVO <LENOVO@192.168.1.9>
2022-01-19 15:26:58 +0530

    first commit

---
+++ src/test/java/JenkinMaven/mavenIntegration/JenkinsTest.java
@@ -0,0 +1,21 @@
+package JenkinMaven.mavenIntegration;
+
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	@Test
+	public void tc1() {
+		System.out.println("Test case one");
+		
+	}
+	@Test
+	public void tc2() {
+		System.out.println("Test case two");
+	}
+		@Test
+		public void tc3() {
+			System.out.println("Test case third");
+		
+	}
+
+}
